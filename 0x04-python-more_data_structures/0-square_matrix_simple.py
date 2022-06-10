8e232799c0c85189e9698693a7ae4ec40230e4d6
Mwende <muthianisharlinemwende@gmail.com>
2022-06-10 14:03:28 +0300

    compute square value of all integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for x in matrix:
+        for y in x:
+            new_matrix.append(y**2)
+    return new_matrix
