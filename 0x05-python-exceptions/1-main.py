1e41f67dfbf6af0926beddfc326e8f135cf75e91
Mwende <muthianisharlinemwende@gmail.com>
2022-06-20 15:23:01 +0300

    safe division

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -11,12 +11,7 @@ has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
     
-value = "Holberton"
+value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-[A[C[C[C[C[C[[A[C[C[C[C[A[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
