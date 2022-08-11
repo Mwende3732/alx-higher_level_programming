2c6fd69630708988b22ff502ebbef95fb662a66a
Mwende <muthianisharlinemwende@gmail.com>
2022-08-11 23:56:02 +0300

    tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,10 @@
+-- Creates a new database
+-- Query to create the database hbtn_0c_0 in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+[A[C[C[C[C[C[A[C[C[C[C[C[[A[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C
