b111fbeb416bd4726616c147c33440ddcf49e67e
Mwende <muthianisharlinemwende@gmail.com>
2022-09-06 15:19:54 +0300

    task 0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const myVar = 'Javascript is amazing';
 console.log(myVar);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-[A[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[
