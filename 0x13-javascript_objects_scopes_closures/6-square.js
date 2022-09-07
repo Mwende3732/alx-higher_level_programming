d439021ad924d7fcb2846b8f89c532ec6ab68335
Mwende <muthianisharlinemwende@gmail.com>
2022-09-07 22:45:20 +0300

    objects in js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+h; j++) {
+        s += c;
+      }
+      console.lo  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.widtg(s);
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+e ((len - i) > 0) {
+    const aux = list[len];
+     let len = list.length - 1;
+  let i = 0;
+  whil list[len] = list[i];
+    list[i] = aux;
+    i++;
+    len--;
+  }
+  return list;
+};
