67fdf21428161ff3d0477852bf7a72e7a710b705
Mwende <muthianisharlinemwende@gmail.com>
2022-09-23 21:17:55 +0300

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request and displays
-the value of the X-Request-Id variable found in the header
-"""
+"""takes in a url, displays the value of the X-request-id variable
+found in header response"""
 import urllib.request
 import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.info()
-          print(html.get('X-Request-Id'))
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
+        
