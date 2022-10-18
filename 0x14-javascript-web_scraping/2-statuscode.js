08de556d86b803ed667637a2fd791bf1f311db94
Mwende <muthianisharlinemwende@gmail.com>
2022-10-18 21:50:05 +0300

    tasks

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+se) {
+  if (error == null) {
+    console.log('coderequest(process.argv[2], function (error, respon: ' + response.statusCode);
+  }
+});
